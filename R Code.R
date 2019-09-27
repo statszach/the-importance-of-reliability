c05e15dbf9fc10fc4b84dc3c7cdddbdf9f350ae4
Kunicki <zkunicki@ad.brown.edu>
2019-09-27 10:52:18 -0400

    wrote function

--- R Code.R
+++ R Code.R
@@ -11,6 +11,53 @@ library(MASS)
 library(faux)
 library(tidyverse)
 
+
+
+## Writing Function ##
+
+reliability_test <- function(n, r){
+  set.seed(2019)
+  data = mvrnorm(n=n, mu=c(0, 0), Sigma=matrix(c(1, r, r, 1), nrow=2), empirical=TRUE)
+  X = data[, 1]  # standard normal (mu=0, sd=1)
+  Y = data[, 2]  # standard normal (mu=0, sd=1)
+  
+  df <- cbind(X, Y)
+  
+  df <- as_tibble(df)
+  
+  prob = c(.1, .2, .4, .2, .1)
+  
+  df <- df %>% mutate(X_Ordinal = norm2likert(X, prob = prob),
+                      Y_Ordinal = norm2likert(Y, prob = prob))
+  
+  diff <- df %>% mutate(D = X_Ordinal - Y_Ordinal) 
+  
+  diff_results <- diff %>% count(D)
+  
+  reliability_valid_results <- diff_results %>% filter(between(D, -1, 1)) 
+  n <- 0
+  reliability_valid <- reliability_valid_results %>% sum(n)/N
+  
+  reliability_notvalid_results <- diff_results %>% filter(!between(D, -1, 1))
+  
+  reliability_notvalid <- reliability_notvalid_results %>% sum(n)/N
+  
+  print(diff_results)
+  sprintf("proportion of valid responses is: %g; proportion of invalid responses is: %g", reliability_valid, reliability_notvalid)
+}
+
+reliability_test(20000, .8)
+
+
+
+
+
+
+
+
+
+
+
 ## Simulating Data with a Pre-Specified Correlation ##
 
 set.seed(2019)
