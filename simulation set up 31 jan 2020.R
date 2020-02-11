554304011d0efd7211dba05fd19852be9c2a4de0
Kunicki <zkunicki@ad.brown.edu>
2020-02-11 11:22:24 -0500

    write four simulation functions

--- simulation set up 31 jan 2020.R
+++ simulation set up 31 jan 2020.R
@@ -5,6 +5,7 @@ library(faux)
 library(psych)
 library(lavaan)
 library(mirt)
+library(Hmisc)
 
 theta <- rnorm(200000, 0, 1)
 
@@ -27,22 +28,6 @@ bind_rows(iterate)
 
 iterate <- as.data.frame(iterate)
 
-
-
-simulate_intconsis <- function(nitems, sva, lambda){
-  theta <- rnorm(200000, 0, 1)
-
-  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
-  
-  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
-  bind_rows(iterate)
-  
-  iterate <- as.data.frame(iterate)
-  
-    
-}
-
-
 test1 <- simulate_intconsis(10, .8, .5)
 
 #This code converts the data into 5-point likert-type data
@@ -54,6 +39,146 @@ test2 <- test1 %>% transmute_all(~norm2likert(., prob = prob_5cat))
 
 test3 <- test1 %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
 
-omega(test2, plot = F, maxit = 10000, fm = "wls")
+omegatest <- omega(test2, plot = F, maxit = 10000, fm = "wls")
 
 omega(test3, plot = F, maxit = 10000, fm = "wls")
+
+simulate_intconsis <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate)
+  
+  iterate <- as.data.frame(iterate)
+  
+
+}
+
+simulate_intconsis_likertnorm <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate)
+  
+  iterate <- as.data.frame(iterate)
+  
+  prob_5cat <- c(.1, .2, .4, .2, .1)
+  
+  test1 <- iterate %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  omegatest <- omega(test1, plot = F, maxit = 10000, fm = "wls")
+  
+  result <- c("omega" = omegatest$omega.tot, "alpha" = omegatest$alpha)
+  
+  return(result)
+}
+
+simulate_intconsis_likertnorm(10, .8, .5)
+
+simulate_intconsis_likertskew <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate)
+  
+  iterate <- as.data.frame(iterate)
+  
+  prob_5cat <- c(.4, .25, .18, .13, .04)
+  
+  test1 <- iterate %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  omegatest <- omega(test1, plot = F, maxit = 10000, fm = "wls")
+  
+  result <- c("omega" = omegatest$omega.tot, "alpha" = omegatest$alpha)
+  
+  return(result)
+}
+
+simulate_intconsis_likertskew(10, .8, .5)
+
+simulate_intconsis_dichot5050 <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate)
+  
+  iterate <- as.data.frame(iterate)
+  
+  test1 <- iterate %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  omegatest <- omega(test1, plot = F, maxit = 10000, fm = "wls")
+  
+  result <- c("omega" = omegatest$omega.tot, "alpha" = omegatest$alpha)
+  
+  return(result)
+}
+
+
+
+simulate_intconsis_dichot5050(10, .8, .5)
+
+
+simulate_intconsis_dichot8020 <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate)
+  
+  iterate <- as.data.frame(iterate)
+  
+  test1 <- iterate %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  omegatest <- omega(test1, plot = F, maxit = 10000, fm = "wls")
+  
+  result <- c("omega" = omegatest$omega.tot, "alpha" = omegatest$alpha)
+  
+  return(result)
+}
+
+
+simulate_intconsis_dichot8020(10, .8, .5)
+
+
+simulate_agreement_likertnorm <- function(nitems, sva, lambda){
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  prob_5cat <- c(.1, .2, .4, .2, .1)
+  
+  test1 <- iterate1 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+
+}
+
+testx <- simulate_agreement_likertnorm(10, .8, .5)
