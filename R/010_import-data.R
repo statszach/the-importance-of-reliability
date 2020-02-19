27e4623b4e127598104bccf3040a46b0cf4d19f9
Kunicki <zkunicki@ad.brown.edu>
2020-02-19 09:36:12 -0500

    coded up to 10 item, 15/20/25 remain

--- R/010_import-data.R
+++ R/010_import-data.R
@@ -36,9 +36,129 @@ repeatability_agreement_likertnorm <- function(r){
   
   reliability_nottrue <- sum(reliability_nottrue_results$n)/obs
   
-  diff_results <- c("absolute reliability" = reliability_absolute, "approximate reliability" = reliability_approx,
-                    "loose reliability" = reliability_nottrue)
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
 
+  return(diff_results)
+}
+
+repeatability_agreement_likertskew <- function(r){
+  obs = 200000
+  data = MASS::mvrnorm(n=obs, mu=c(0, 0), Sigma=matrix(c(1, r, r, 1), nrow=2), empirical=TRUE)
+  X = data[, 1]  # standard normal (mu=0, sd=1)
+  Y = data[, 2]  # standard normal (mu=0, sd=1)
+  
+  df <- cbind(X, Y)
+  
+  df <- as_tibble(df)
+  
+  prob = c(.4, .25, .18, .13, .04)
+  
+  df <- df %>% mutate(X_Ordinal = norm2likert(X, prob = prob),
+                      Y_Ordinal = norm2likert(Y, prob = prob))
+  
+  df$D <- df$X_Ordinal - df$Y_Ordinal
+  
+  df_results <- df %>% count(D)
+  
+  reliability_absolute_results <- df_results %>% filter(D == 0) 
+  reliability_absolute_results
+  
+  reliability_absolute <- sum(reliability_absolute_results$n)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- df_results %>% filter(between(D, -1, 1)) 
+  reliability_approx_results
+  
+  reliability_approx <- sum(reliability_approx_results$n)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- df_results %>% filter(!between(D, -1, 1))
+  reliability_nottrue_results
+  
+  reliability_nottrue <- sum(reliability_nottrue_results$n)/obs
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  
+  return(diff_results)
+}
+
+repeatability_agreement_dichot5050 <- function(r){
+  obs = 200000
+  data = MASS::mvrnorm(n=obs, mu=c(0, 0), Sigma=matrix(c(1, r, r, 1), nrow=2), empirical=TRUE)
+  X = data[, 1]  # standard normal (mu=0, sd=1)
+  Y = data[, 2]  # standard normal (mu=0, sd=1)
+  
+  df <- cbind(X, Y)
+  
+  df <- as_tibble(df)
+  
+  df <- df %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  df$D <- df$X - df$Y
+  
+  df_results <- df %>% count(D)
+  
+  reliability_absolute_results <- df_results %>% filter(D == 0) 
+  reliability_absolute_results
+  
+  reliability_absolute <- sum(reliability_absolute_results$n)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- df_results %>% filter(between(D, -1, 1)) 
+  reliability_approx_results
+  
+  reliability_approx <- sum(reliability_approx_results$n)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- df_results %>% filter(!between(D, -1, 1))
+  reliability_nottrue_results
+  
+  reliability_nottrue <- sum(reliability_nottrue_results$n)/obs
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  
+  return(diff_results)
+}
+
+repeatability_agreement_dichot8020 <- function(r){
+  obs = 200000
+  data = MASS::mvrnorm(n=obs, mu=c(0, 0), Sigma=matrix(c(1, r, r, 1), nrow=2), empirical=TRUE)
+  X = data[, 1]  # standard normal (mu=0, sd=1)
+  Y = data[, 2]  # standard normal (mu=0, sd=1)
+  
+  df <- cbind(X, Y)
+  
+  df <- as_tibble(df)
+  
+  df <- df %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  df$D <- df$X - df$Y
+  
+  df_results <- df %>% count(D)
+  
+  reliability_absolute_results <- df_results %>% filter(D == 0) 
+  reliability_absolute_results
+  
+  reliability_absolute <- sum(reliability_absolute_results$n)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- df_results %>% filter(between(D, -1, 1)) 
+  reliability_approx_results
+  
+  reliability_approx <- sum(reliability_approx_results$n)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- df_results %>% filter(!between(D, -1, 1))
+  reliability_nottrue_results
+  
+  reliability_nottrue <- sum(reliability_nottrue_results$n)/obs
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  
   return(diff_results)
 }
 
@@ -131,7 +251,13 @@ simulate_intconsis_dichot8020 <- function(nitems, sva, lambda){
 
 #The functions below calculate agreement based on the internal consistency simulation.
 
-simulate_agreement_likertnorm <- function(nitems, sva, lambda){
+# 5 items
+
+simulate_agreement_likertnorm_5item <- function(sva, lambda){
+  
+  nitems = 5
+  obs = 200000
+  
   theta <- rnorm(200000, 0, 1)
   
   SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
@@ -160,5 +286,378 @@ simulate_agreement_likertnorm <- function(nitems, sva, lambda){
   
   discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
   
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 1)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 2, 4)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 4)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_likertskew_5item <- function(sva, lambda){
+  
+  nitems = 5
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  prob_5cat <- c(.4, .25, .18, .13, .04)
+  
+  test1 <- iterate1 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 1)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 2, 4)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 4)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_dichot5050_5item <- function(sva, lambda){
+  
+  nitems = 5
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+
+  test1 <- iterate1 %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d == 0)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 1, 2)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 2)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_dichot8020_5item <- function(sva, lambda){
+  
+  nitems = 5
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  test1 <- iterate1 %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d == 0)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 1, 2)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 2)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+# 10 item
+
+simulate_agreement_likertnorm_10item <- function(sva, lambda){
+  
+  nitems = 10
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  prob_5cat <- c(.1, .2, .4, .2, .1)
+  
+  test1 <- iterate1 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 3)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 4, 8)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 8)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_likertskew_10item <- function(sva, lambda){
+  
+  nitems = 10
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  prob_5cat <- c(.4, .25, .18, .13, .04)
+  
+  test1 <- iterate1 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~norm2likert(., prob = prob_5cat))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 3)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 4, 8)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 8)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_dichot5050_10item <- function(sva, lambda){
+  
+  nitems = 10
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  test1 <- iterate1 %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~dplyr::if_else(. > 0, 1, 0))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 1)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 1, 2)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 2)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue
+  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
+  
+}
+
+simulate_agreement_dichot8020_10item <- function(sva, lambda){
+  
+  nitems = 10
+  obs = 200000
+  
+  theta <- rnorm(200000, 0, 1)
+  
+  SVA_theta <- theta * sva + sqrt((1 - sva^2)) * rnorm(200000, 0, 1)  
+  
+  iterate1 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate1)
+  
+  iterate1 <- as.data.frame(iterate1)
+  
+  test1 <- iterate1 %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  sum1 <- test1 %>% transmute(sum1 = rowSums(.))
+  
+  iterate2 <- map(1:nitems, ~SVA_theta * lambda + sqrt((1-lambda^2)) * rnorm(200000, 0, 1)) %>% set_names(letters[1:nitems])
+  bind_rows(iterate2)
+  
+  iterate2 <- as.data.frame(iterate2)
+  
+  test2 <- iterate2 %>% transmute_all(~dplyr::if_else(. > -.84, 1, 0))
+  
+  sum2 <- test2 %>% transmute(sum2 = rowSums(.))
+  
+  d_data <- cbind.data.frame(sum1, sum2)
+  
+  discrepancy_data <- d_data %>% mutate(d = sqrt((sum1-sum2)^2))
+  
+  reliability_absolute_results <- discrepancy_data %>% filter(d <= 1)
+  reliability_absolute <- length(reliability_absolute_results$d)/obs
+  reliability_absolute
+  
+  reliability_approx_results <- discrepancy_data %>% filter(between(d, 1, 2)) 
+  reliability_approx <- length(reliability_approx_results$d)/obs
+  reliability_approx
+  
+  reliability_nottrue_results <- discrepancy_data %>% filter(d > 2)
+  reliability_nottrue <- length(reliability_nottrue_results$d)/obs
+  reliability_nottrue  
+  
+  diff_results <- c("absolute agreement" = reliability_absolute, "approximate agreement" = reliability_approx,
+                    "loose agreement" = reliability_nottrue)
+  return(diff_results)
   
 }
